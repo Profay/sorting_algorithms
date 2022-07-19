8f70a0a0a19f9be90d466df45d7065195c9bc6db
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-19 19:45:32 +0000

    update buble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,49 +1,38 @@
 #include "sort.h"
-
 /**
- * swap - swap position of the elements passed
- *
- * @a: pointer the element one
- * @b: pointer the element two
+ * bubble_sort - comparing 1st & 2nd elements to check which one is greater
+ * @array:list of number
+ * @size:size of array
+ * Return: None
  */
-
-void swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * bubble_sort - Sorts an array using the Bubble sort algorithm
- *
- * @array: Array to be sorted
- * @size: Size of the array
- */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, flag;
+	size_t i, j;
 
 	if (size < 2)
 		return;
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			flag = 0;
-			for (j = 0; j < size - 1 - i; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					swap(&array[j], &array[j + 1]);
-					print_array(array, size);
-					flag = 1;
-				}
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
 			}
-			if (flag == 0)
-				break;
 		}
 	}
+}
+/**
+ * swap - swap the 2 elements
+ * @x:element
+ * @y:element
+ * Return: None
+ */
+void swap(int *x, int *y)
+{
+	int temp = *x;
+
+	*x = *y;
+	*y = temp;
 }
