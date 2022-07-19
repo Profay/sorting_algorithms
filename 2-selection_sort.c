5ad3acf83eba4187f37adcfbfe40035a4a842184
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-19 14:23:07 +0000

    Creating time complexity for selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,14 +23,18 @@
 
  void selection_sort(int *array, size_t size) {
     size_t i, j, minimum;
-    for (i = 0; i < size - 1; i++) {
-        minimum = i;
-        for (j = i + 1; j < size; j++) {
-            if (array[j] < array[minimum]) {
-                minimum = j;
+    if (size >= 2){
+        for (i = 0; i < size - 1; i++) {
+            minimum = i;
+            for (j = i + 1; j < size; j++) {
+                if (array[j] < array[minimum]) {
+                    minimum = j;
+                }
+            }
+            if (minimum != i) {
+            swap(&array[i], &array[minimum]);
+            print_array(array, size);
             }
         }
-        swap(&array[i], &array[minimum]);
-        print_array(array, size);
     }
  }
