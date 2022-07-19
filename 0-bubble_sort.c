ba1ea10d82f0eb5f11d75b0825f0d0c3b52f28b9
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-19 13:41:45 +0000

    correcting betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,48 @@
 #include "sort.h"
 
-
 /**
- * bubble_sort - Function to sort a list of objects
- * @array: list of objects
- * @size: size of the list
+ * swap - swap position of the elements passed
+ *
+ * @a: pointer the element one
+ * @b: pointer the element two
  */
 
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
- void bubble_sort(int *array, size_t size)
- {
-    size_t i, j, flag;
-    if (size >= 2)
-    for (i = 0; i < size - 1; ++i) {
-        flag = 0;
-        for(j = 0; j < size - i -1; ++j) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);
-                flag = 1;
-                }
-            }
-            if (flag == 0) {
-                break;
-            }
-    }
- }
+/**
+ * bubble_sort - Sorts an array using the Bubble sort algorithm
+ *
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, flag;
+
+	if (size >= 2)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			flag = 0;
+			for (j = 0; j < size - 1 - i; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+					flag = 1;
+				}
+			}
+			if (flag == 0)
+				break;
+		}
+	}
+}
