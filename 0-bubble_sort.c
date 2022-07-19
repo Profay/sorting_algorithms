3638b5c34abd559db3a8a2d820bd4b1b0b5f4046
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-19 20:16:54 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,18 +9,21 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int max;
-
-	for (i = size; i > 1; i--)
-	{
-		for (j = 0; j < i - 1; j++)
+	if (size < 2){
+		return;
+	}
+	else{
+		for (i = size; i > 1; i--)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < i - 1; j++)
 			{
-				max = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = max;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					max = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = max;
+					print_array(array, size);
+				}
 			}
 		}
-	}
 }
