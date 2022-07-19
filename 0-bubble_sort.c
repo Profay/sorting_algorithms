0c033aa5a7812c39efebd9f8e18767ad97dd73b5
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-19 13:38:57 +0000

    updTE

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,13 +16,19 @@ void swap(int *a, int *b)
 
  void bubble_sort(int *array, size_t size)
  {
-    size_t i, j;
+    size_t i, j, flag;
     for (i = 0; i < size - 1; ++i) {
+        flag = 0;
         for(j = 0; j < size - i -1; ++j) {
             if (array[j] > array[j + 1]) {
                 swap(&array[j], &array[j + 1]);
                 print_array(array, size);
+                flag = 1;
+                }
             }
+            if (flag == 0) {
+                break;
             }
+    }
  }
- }
+ 
